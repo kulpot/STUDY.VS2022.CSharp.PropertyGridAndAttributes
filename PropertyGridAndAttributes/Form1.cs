bb65849888d13ff121769adf0b54ceb835258688
Kulpot <kulpotwahu@gmail.com>
2023-05-21 06:31:34 +0800

    Add project files.

---
+++ PropertyGridAndAttributes/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PropertyGridAndAttributes
+{
+    public partial class Form1 : Form
+    {
+        private MyClass _myClass = new MyClass();
+
+        public Form1()
+        {
+            InitializeComponent();
+            propertyGrid1.SelectedObject = _myClass;
+        }
+    }
+}
---
+++ PropertyGridAndAttributes/MyClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyGridAndAttributes
+{
+    class MyClass
+    {
+        // Set the same values here or in the constructor as the default values
+        private bool _isLookingNice = true;
+        private string _errorMessage = "Because I said so";
+
+        [DefaultValue(true)]
+        [Category("Apperance")]
+        public bool IsLookingNice
+        {
+            get { return _isLookingNice; }
+            set { _isLookingNice = value; }
+        }
+
+        [Browsable(false)]
+        [DisplayName("Error Message 545")]
+        [Description("Gets the message to display when error 545 occurs")]
+        [DefaultValue("Because I said so")]
+        [Category("Prompting")]
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set { _errorMessage = value; }
+        }
+    }
+}
